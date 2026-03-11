0d1db0a5ae6ac81c2b744339c914fb0a552724c1
Miquel <M.RicartIOltra@student.tudelft.nl>
2026-03-11 19:52:58 +0100

    parking exercises added vehicles and parking spot

--- easy_design_parking_lot/main.cpp
+++ easy_design_parking_lot/main.cpp
@@ -1,29 +1,6 @@
 #include <vector>
 #include <string>
-
-class Vehicle
-{
-    private:
-        VehicleSize size_;
-        std::string licensePlate_;
-    public:
-        virtual ~Vehicle(){};
-
-};
-
-class Motorcycle : Vehicle
-{
-    public:
-        Motorcycle(VehicleSize size, std::string licenseplate)
-        : size_{size}, licensePlate_{licenseplate} 
-        {};
-};
-
-class Car : Vehicle
-{};
-
-class Truck : Vehicle
-{};
+#include <stdexcept>
 
 enum class VehicleSize
 {
@@ -32,9 +9,71 @@ enum class VehicleSize
     LARGE
 };
 
+class Vehicle
+{
+    private:
+        VehicleSize size_;
+        std::string licensePlate_;
+    public:
+        Vehicle(VehicleSize size, std::string licensePlate) 
+        : size_(size), licensePlate_(licensePlate) {}
+
+        virtual ~Vehicle(){};
+        VehicleSize GetSize() const {return size_;}
+        std::string GetLicensePlate() const {return licensePlate_;}
+};
+
+class Motorcycle : public Vehicle
+{
+    public:
+        Motorcycle(std::string licensePlate)
+        : Vehicle(VehicleSize::SMALL, licensePlate) {};
+};
+
+class Car : Vehicle
+{
+    public:
+        Car(std::string licensePlate)
+        : Vehicle(VehicleSize::MEDIUM, licensePlate) {};
+};
+
+class Truck : Vehicle
+{
+    public:
+        Truck(std::string licensePlate)
+        : Vehicle(VehicleSize::LARGE, licensePlate) {};
+};
+
+
+
 class ParkingSpot
 {
     private:
+        VehicleSize spotSize_;
+        std::string spotId_;
+        bool isOccupied_;
+        Vehicle* parkedVehicle_;
+    
+    public:
+        ParkingSpot(VehicleSize size, std::string id)
+        : spotSize_{size}, spotId_{id}, isOccupied_{false}, parkedVehicle_{nullptr} {};
+
+        void parkVehicle(Vehicle *vehicle)
+        {
+            if (vehicle == nullptr) throw std::invalid_argument("Cannot park a non-existent vehicle.");
+            if (vehicle->GetSize() != spotSize_) throw std::invalid_argument("Vehicle size does not match parking spot size.");
+            if (isOccupied_) throw std::logic_error("Parking spot is already occupied.");
+            isOccupied_ = true;
+            parkedVehicle_ = vehicle;
+        }
+
+        void freeSpace()
+        {
+            isOccupied_ = false;
+            parkedVehicle_ = nullptr;
+        }
+
+        bool isAvailable() {return !isOccupied_;}
 
 };
 
