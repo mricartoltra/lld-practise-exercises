860175e55fae550d2270ba22b3aaf54c166b9ae0
Miquel <M.RicartIOltra@student.tudelft.nl>
2026-03-13 23:40:11 +0100

    vending machine added states

--- easy_vending_machine/VendingMachineStates.cpp
+++ easy_vending_machine/VendingMachineStates.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <unordered_map>
 #include <stdexcept>
+#include <iostream>
 
 class Inventory
 {
@@ -44,22 +45,40 @@ class WaitingProductState : public State
 class WaitingMoneyState : public State
 {
     private:
+        std::string id_;
         int balance = 0;
     public:
-        WaitingMoneyState() : State() {}
-        void SelectProduct(std::string id) override {};
-        void AddMoney(int money) override {};
-        void DeliverProduct() override {};
-        void RefundMoney() override {};
+        WaitingMoneyState(std::string id) : State(), id_{id} {}
+        void SelectProduct(std::string id) override {
+            throw std::invalid_argument("Already Selected");
+        };
+        void AddMoney(int money) override {
+            balance += money;
+        };
+        void DeliverProduct() override {
+            //if balance is >= then deliver and return extra money
+        };
+        void RefundMoney() override {
+            std::cout<<"Returned "<<balance<<" euros."<<std::endl;
+            balance = 0;
+        };
 };
 class DeliveringProductState : public State
 {
     public:
         DeliveringProductState() : State() {}
-        void SelectProduct(std::string id) override {};
-        void AddMoney(int money) override {};
-        void DeliverProduct() override {};
-        void RefundMoney() override {};
+        void SelectProduct(std::string id) override {
+            throw std::invalid_argument("Processing");
+        };
+        void AddMoney(int money) override {
+            throw std::invalid_argument("Processing");
+        };
+        void DeliverProduct() override {
+            throw std::invalid_argument("Processing");
+        };
+        void RefundMoney() override {
+            throw std::invalid_argument("Processing");
+        };
 };
 
 
@@ -80,9 +99,15 @@ class VendingMachine
         void AddStock(){}
         
         //state dependant
-        void SelectProduct(std::string id){}
-        void AddMoney(){}
-        void DeliverProduct(){}
+        void SelectProduct(std::string id){
+            state_->SelectProduct(id);
+        }
+        void AddMoney(int money){
+            state_->AddMoney(money);
+        }
+        void DeliverProduct(){
+            state_->DeliverProduct();
+        }
         void RefundMoney(){}
 };
 
