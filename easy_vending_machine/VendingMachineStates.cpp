442407da865d2ab73b07d9c7cc8f7a41084b25c5
Miquel <M.RicartIOltra@student.tudelft.nl>
2026-03-13 21:08:22 +0100

    vending machine starting version with states

---
+++ easy_vending_machine/VendingMachineStates.cpp
@@ -0,0 +1,96 @@
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <stdexcept>
+
+class Inventory
+{
+    private:
+        std::unordered_map<std::string, int> idToStock;
+
+    public:
+        Inventory(){}
+};
+
+class State
+{
+    public:
+        State(){}
+        virtual ~State(){};
+        virtual void SelectProduct(std::string id) = 0;
+        virtual void AddMoney(int money) = 0;
+        virtual void DeliverProduct() = 0;
+        virtual void RefundMoney() = 0;
+
+
+};
+class WaitingProductState : public State
+{
+    public:
+        WaitingProductState() : State() {}
+        void SelectProduct(std::string id) override {
+            //
+        };
+        void AddMoney(int money) override {
+            throw std::invalid_argument("No Product Selected");
+        };
+        void DeliverProduct() override {
+            throw std::invalid_argument("No Product Selected");
+        };
+        void RefundMoney() override {
+            throw std::invalid_argument("No Money to return");
+        };
+};
+class WaitingMoneyState : public State
+{
+    private:
+        int balance = 0;
+    public:
+        WaitingMoneyState() : State() {}
+        void SelectProduct(std::string id) override {};
+        void AddMoney(int money) override {};
+        void DeliverProduct() override {};
+        void RefundMoney() override {};
+};
+class DeliveringProductState : public State
+{
+    public:
+        DeliveringProductState() : State() {}
+        void SelectProduct(std::string id) override {};
+        void AddMoney(int money) override {};
+        void DeliverProduct() override {};
+        void RefundMoney() override {};
+};
+
+
+class VendingMachine
+{
+    private:
+        Inventory* inventory_;
+        State* state_;
+
+    public:
+        VendingMachine(Inventory* inventory)
+        : inventory_{inventory}
+        {
+            state_ = new WaitingProductState();
+        }
+
+        void AddProduct(std::string id, std::string name, int price){}
+        void AddStock(){}
+        
+        //state dependant
+        void SelectProduct(std::string id){}
+        void AddMoney(){}
+        void DeliverProduct(){}
+        void RefundMoney(){}
+};
+
+int main()
+{
+    std::unique_ptr<Inventory> inventory = std::make_unique<Inventory>();
+    std::unique_ptr<VendingMachine> vendingMachine = std::make_unique<VendingMachine>(inventory);
+
+
+    return 0;
+}
